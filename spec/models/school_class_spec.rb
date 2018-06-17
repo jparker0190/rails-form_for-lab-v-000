7ad85273661771098f6f04419f22d3d777370257
Joseph Parker <jparker010190@gmail.com>
2018-06-17 20:24:52 +0000

    automatically backed up by learn

--- spec/models/school_class_spec.rb
+++ spec/models/school_class_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe SchoolClass do
   before(:each) do
-    @school_class = SchoolClass.create!(title: "Computer Science", room_number: 5)
+    @school_class = SchoolsClass.create!(title: "Computer Science", room_number: 5)
   end
 
   it 'can be created' do
