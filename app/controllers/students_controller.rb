9509d7c727141d6a7f7ad10907576206c4870112
Joseph Parker <jparker010190@gmail.com>
2018-06-17 19:59:37 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,7 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new(student_params(:first_name, :last_name))
   end
 
   def edit
@@ -11,6 +12,7 @@ class StudentsController < ApplicationController
   def update
     @student = Student.find(params[:id])
     @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path (@student)
   end
   private
   def student_params(*arg)
