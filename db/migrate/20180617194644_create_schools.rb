b5fa7b9311e27d057a1f0744c4f3c9e8582d3271
Joseph Parker <jparker010190@gmail.com>
2018-06-17 20:21:47 +0000

    automatically backed up by learn

--- db/migrate/20180617194644_create_schools.rb
+++ db/migrate/20180617194644_create_schools.rb
@@ -1,6 +1,6 @@
 class CreateSchools < ActiveRecord::Migration
   def change
-    create_table :schools_classes do |t|
+    create_table :school_classes do |t|
       t.string :title
       t.integer :room_number
 
