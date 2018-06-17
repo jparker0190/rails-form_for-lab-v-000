f2bf6a861392feae0b971d33db6567fd470951ce
Joseph Parker <jparker010190@gmail.com>
2018-06-17 20:30:15 +0000

    automatically backed up by learn

--- spec/features/schools_class_spec.rb
+++ spec/features/schools_class_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 
 describe 'form page' do
   it 'form renders with the new action' do
-    visit new_school_class_path
+    visit new_schools_class_path
     expect(page).to have_content("School Class Form")
   end
 
   it 'new form submits content and renders form content' do
-    visit new_school_class_path
+    visit new_schools_class_path
 
-    fill_in 'school_class_title', with: "Software Engineering"
-    fill_in 'school_class_room_number', with: 10
+    fill_in 'schools_class_title', with: "Software Engineering"
+    fill_in 'schools_class_room_number', with: 10
 
     click_on "Create School class"
 
@@ -19,12 +19,12 @@ describe 'form page' do
   end
 
   it 'edit form submits content and renders form content' do
-    @edit_school_class = SchoolClass.create(title: "Computer Science", room_number: 5)
+    @edit_schools_class = SchoolClass.create(title: "Computer Science", room_number: 5)
 
-    visit edit_school_class_path(@edit_school_class)
+    visit edit_schools_class_path(@edit_schools_class)
 
-    fill_in 'school_class_title', with: "Risk Analysis"
-    fill_in 'school_class_room_number', with: 10
+    fill_in 'schools_class_title', with: "Risk Analysis"
+    fill_in 'schools_class_room_number', with: 10
 
     click_on "Update School class"
 
@@ -32,10 +32,10 @@ describe 'form page' do
   end
 
   it 'submitted new form creates a record in the database' do
-    visit new_school_class_path
+    visit new_schools_class_path
 
-    fill_in 'school_class_title', with: "Sabermetrics"
-    fill_in 'school_class_room_number', with: 42
+    fill_in 'schools_class_title', with: "Sabermetrics"
+    fill_in 'schools_class_room_number', with: 42
 
     click_on "Create School class"
 
@@ -43,12 +43,12 @@ describe 'form page' do
   end
 
   it 'submitted edit form creates a record in the database' do
-    @edit_db_school_class = SchoolClass.create(title: "Computer Science", room_number: 5)
+    @edit_db_schools_class = SchoolClass.create(title: "Computer Science", room_number: 5)
 
-    visit edit_school_class_path(@edit_db_school_class)
+    visit edit_schools_class_path(@edit_db_schools_class)
 
-    fill_in 'school_class_title', with: "Machine Learning"
-    fill_in 'school_class_room_number', with: 11
+    fill_in 'schools_class_title', with: "Machine Learning"
+    fill_in 'schools_class_room_number', with: 11
 
     click_on "Update School class"
 
@@ -57,15 +57,15 @@ describe 'form page' do
 end
 
 describe 'Show page' do
-  let(:school_class) { SchoolClass.create(title: "Computer Science", room_number: 5) }
+  let(:schools_class) { SchoolClass.create(title: "Computer Science", room_number: 5) }
 
   it 'renders properly' do
-    visit school_class_path(school_class)
+    visit schools_class_path(schools_class)
     expect(page.status_code).to eq(200)
   end
 
   it 'renders the school class title' do
-    visit school_class_path(school_class)
+    visit schools_class_path(schools_class)
     expect(page).to have_content("Computer Science")
   end
 end
