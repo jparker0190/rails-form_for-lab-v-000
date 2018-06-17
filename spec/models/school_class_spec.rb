03500bb615ce4e8821dd8685d08d71f7df112b31
Joseph Parker <jparker010190@gmail.com>
2018-06-17 20:25:55 +0000

    automatically backed up by learn

--- spec/models/school_class_spec.rb
+++ spec/models/school_class_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe SchoolClass do
+describe SchoolsClass do
   before(:each) do
-    @school_class = SchoolsClass.create!(title: "Computer Science", room_number: 5)
+    @schools_class = SchoolsClass.create!(title: "Computer Science", room_number: 5)
   end
 
   it 'can be created' do
-    expect(@school_class).to be_valid
+    expect(@schools_class).to be_valid
   end
 
 end
