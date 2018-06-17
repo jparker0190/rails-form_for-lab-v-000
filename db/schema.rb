d313d51bd667ec9139da9a9dfc6dae3626cec5ca
Joseph Parker <jparker010190@gmail.com>
2018-06-17 20:42:02 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180617194644) do
+ActiveRecord::Schema.define(version: 2) do
 
-  create_table "schools_classes", force: :cascade do |t|
+  create_table "school_classes", force: :cascade do |t|
     t.string   "title"
     t.integer  "room_number"
     t.datetime "created_at",  null: false
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(version: 20180617194644) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+end
 
 end
